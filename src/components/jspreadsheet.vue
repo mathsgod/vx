77f0279acdcc66b6612bec4bb4a01cc12fd9cf77
Raymond Chong <mathsgod@yahoo.com>
2021-08-26 12:50:47 +0800

    update jspreadsheet

--- src/components/jspreadsheet.vue
+++ src/components/jspreadsheet.vue
@@ -7,6 +7,7 @@ import jspreadsheet from "jspreadsheet-ce";
 
 export default {
   mounted() {
+    console.log(this.$attrs);
     Object.assign(this, jspreadsheet(this.$el, this.$attrs));
   },
 };
--- src/components/vx-test.vue
+++ src/components/vx-test.vue
@@ -2,10 +2,13 @@
   <div>
     abc
 
-    <jspreadsheet :data="data" ref="jss"></jspreadsheet>
+    <jspreadsheet
+      ref="jss"
+      :minDimensions="[5, 10]"
+      :columns="columns"
+    ></jspreadsheet>
 
     <el-button @click="add">add</el-button>
-    
   </div>
 </template>
 <script>
@@ -15,20 +18,39 @@ export default {
   data() {
     return {
       search: "",
-      data: [
-        ["US", "Apples", "Yes", "2019-02-12"],
-        ["CA;US;UK", "Carrots", "Yes", "2019-03-01"],
-        ["CA;BR", "Oranges", "No", "2018-11-10"],
-        ["BR", "Coconuts", "Yes", "2019-01-12"],
+      columns: [
+        {
+          type: "text",
+          title: "Number",
+          width: 100,
+        },
+        {
+          type: "text",
+          title: "Name",
+          width: 500,
+        },
+        {
+          type: "text",
+          title: "Width",
+          width: 100,
+        },
+        {
+          type: "text",
+          title: "Depth",
+          width: 100,
+        },
+        {
+          type: "text",
+          title: "Height",
+          width: 100,
+        },
       ],
     };
   },
   methods: {
     add() {
       this.$refs.jss.insertRow();
-
     },
-    
   },
 };
 </script>
